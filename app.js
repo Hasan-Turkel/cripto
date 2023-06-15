f37c82f3aeafcaff3a68bfc2f61eaba8d2f54930
hasan <mmhotmail>
2023-06-15 18:43:27 +0300

    write dom func commit

--- app.js
+++ app.js
@@ -14,6 +14,22 @@ const options = {
 
 const form = document.querySelector("form")
 const input = document.querySelector("input")
+const main = document.querySelector("main")
+
+
+
+// ******functions*******
+
+const writeDom = (json)=>{
+ const object = json.filter(obj=> obj.name == input.value );
+ main.innerHTML += `<div class="d-flex flex-column col-3 bg-white p-3 rounded-4 shadow" style="width: 150px;">
+ <p>${object.name}</p>
+ <p>${object.name}</p>
+ <img src="" alt="">
+ <p>${object.name}</p>
+</div>`
+
+}
 
 // ******events******
 
